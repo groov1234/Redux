689624029f7e68e56a0f8c1f0e89d1aeca53febe
Calum Groover <30381292+groov1234@users.noreply.github.com>
2018-01-24 11:46:27 -0500

    Add files via upload

--- todo/src/index.js
+++ todo/src/index.js
@@ -8,11 +8,13 @@ import { Provider } from 'react-redux';
 import reducers from './reducers/index.js';
 
 // create redux store
-const store = createStore(reducers);
-
+const store = createStore(reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+  
 store.subscribe(() => {
-  console.log(`STORE HAS CHANGED: `, store.getState());
-});
+  console.log(`STORE HAS CHANGED: `, store.getState())
+  
+})
 
 ReactDOM.render(
   <Provider store={store}>
